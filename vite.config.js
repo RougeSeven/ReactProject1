f8f52bd9faeb0c4c265829f0eb879071297337d0
Marcelo Alejandro Pareja Moreira <155473860+RougeSeven@users.noreply.github.com>
2025-06-18 21:46:12 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
+    host: 0.0.0.0,
     allowedHosts: ['*']
   },
   server: {
-    host: true,
+    host: 0.0.0.0,
     port: 5173,
     allowedHosts: ['*']
   }
